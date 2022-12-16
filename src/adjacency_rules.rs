f7c68973ac4e3ba3ec0895e46fdb48946c54a9f6
Ben Kunkle <ben.kunkle@gmail.com>
2022-12-16 03:05:35 -0800

    preprocessor additions

--- src/adjacency_rules.rs
+++ src/adjacency_rules.rs
@@ -1,10 +1,13 @@
-use glam::IVec2;
-use std::{collections::{HashMap,HashSet}, ops::{Index, IndexMut}, slice::Iter};
-
+use glam::{IVec2, UVec2};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::{Deref, DerefMut, Index, IndexMut, Neg},
+    slice::Iter,
+};
 
 #[derive(Debug, Default)]
 pub struct AdjacencyRules {
-    map: HashMap<usize, [HashSet<usize>;4]>
+    map: HashMap<usize, [HashSet<usize>; 4]>,
 }
 
 impl AdjacencyRules {
@@ -25,45 +28,158 @@ impl AdjacencyRules {
         self.len() == 0
     }
 
+    pub fn allow_bi(&mut self, from: usize, to: usize, dir: CardinalDirs) {
+        self.allow(from, to, dir);
+        self.allow(to, from, -dir);
+    }
+
     pub fn allow(&mut self, from_: usize, to_: usize, dir: CardinalDirs) {
         // self.try_add_new(from_);
-        self.map.entry(from_)
-            .or_insert_with(Default::default)
-            [dir].insert(to_);
+        self.map.entry(from_).or_insert_with(Default::default)[dir].insert(to_);
         // self.map.get_mut(&from_).expect("from_ already in map")[dir].insert(to_);
     }
 
-    pub fn is_allowed(&self, from_: usize, to_:usize, dir: CardinalDirs) -> bool {
-        return self.map[&from_][dir].contains(&to_);
+    pub fn is_allowed(&self, tile_dom_in: usize, q: usize, dir: CardinalDirs) -> bool {
+        return self.map[&tile_dom_in][dir].contains(&q);
+    }
+
+    fn enabled_by(&self, from: usize, dir: CardinalDirs) -> Vec<usize> {
+        return self.map[&from][dir].iter().copied().collect();
+    }
+
+    fn enabled_by_count(&self, from: usize) -> [usize; 4] {
+        let mut counts = [0; 4];
+        let by_dirs: Vec<usize> = CardinalDirs::iter()
+            .map(|&d| self.enabled_by(from, d).len())
+            .collect();
+        counts[0] = by_dirs[0];
+        counts[1] = by_dirs[1];
+        counts[2] = by_dirs[2];
+        counts[3] = by_dirs[3];
+        return counts;
     }
 
     #[cfg(test)]
     pub fn allowed_in_all_dirs(&self, from_: usize, to_: usize) -> bool {
-        return CardinalDirs::iter().map(|&dir| self.is_allowed(from_, to_, dir)).all(|b| b);
+        return CardinalDirs::iter()
+            .map(|&dir| self.is_allowed(from_, to_, dir))
+            .all(|b| b);
     }
 }
 
-#[derive(Debug,Clone,Copy)]
+type Enabled = [usize; 4];
+
+#[derive(Debug, Clone)]
+pub struct EnablerDict(Vec<Enabled>);
+
+impl Deref for EnablerDict {
+    type Target = Vec<Enabled>;
+
+    fn deref(&self) -> &Self::Target {
+        return &self.0;
+    }
+}
+
+impl DerefMut for EnablerDict {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        return &mut self.0;
+    }
+}
+
+impl EnablerDict {
+    pub fn new(adjacency_rules: &AdjacencyRules) -> Self {
+        let enablers = adjacency_rules
+            .map
+            .keys()
+            .map(|&id| adjacency_rules.enabled_by_count(id))
+            .collect();
+        return Self(enablers);
+    }
+
+    pub fn remove(
+        &mut self,
+        from: usize,
+        cell_loc: UVec2,
+        dir: CardinalDirs,
+        adjacency_rules: &AdjacencyRules,
+    ) -> Option<Vec<TileRemovalEvent>> {
+        let enabled = adjacency_rules.enabled_by(from, dir);
+        let events: Vec<TileRemovalEvent> = enabled
+            .iter()
+            .filter_map(|&id| {
+                let count = self[id][dir];
+                match count {
+                    1 => {
+                        // decrementing this tile would remove all enablers in a dir
+                        // making the possibility of this tile impossible
+                        // remove the tile completely
+                        self[id] = [0; 4];
+                        // save event for change propogation
+                        Some(TileRemovalEvent {
+                            tile_id: from,
+                            cell_loc,
+                        })
+                    }
+                    // event is already removed
+                    // do nothing
+                    0 => None,
+                    // event has enablers
+                    // decrement enabler count
+                    c => {
+                        self[id][dir] = c - 1;
+                        None
+                    }
+                }
+            })
+            .collect();
+        if events.is_empty() {
+            return None;
+        } else {
+            return Some(events);
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TileRemovalEvent {
+    pub tile_id: usize,
+    pub cell_loc: UVec2,
+}
+
+#[derive(Debug, Clone, Copy)]
 pub enum CardinalDirs {
-    UP,
-    LEFT,
-    RIGHT,
-    DOWN,
+    Up,
+    Left,
+    Right,
+    Down,
 }
 
 impl CardinalDirs {
     pub fn iter() -> Iter<'static, Self> {
-        return [Self::UP, Self::LEFT, Self::DOWN, Self::RIGHT].iter();
+        return [Self::Up, Self::Left, Self::Down, Self::Right].iter();
+    }
+}
+
+impl Neg for CardinalDirs {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        match self {
+            CardinalDirs::Up => Self::Down,
+            CardinalDirs::Left => Self::Right,
+            CardinalDirs::Right => Self::Left,
+            CardinalDirs::Down => Self::Up,
+        }
     }
 }
 
 impl From<CardinalDirs> for usize {
     fn from(value: CardinalDirs) -> Self {
         match value {
-            CardinalDirs::UP => 0,
-            CardinalDirs::LEFT => 1,
-            CardinalDirs::RIGHT => 2,
-            CardinalDirs::DOWN => 3,
+            CardinalDirs::Up => 0,
+            CardinalDirs::Left => 1,
+            CardinalDirs::Right => 2,
+            CardinalDirs::Down => 3,
         }
     }
 }
@@ -71,15 +187,15 @@ impl From<CardinalDirs> for usize {
 impl From<CardinalDirs> for IVec2 {
     fn from(value: CardinalDirs) -> Self {
         match value {
-            UP => IVec2 {x:0,y:1},
-            LEFT => IVec2 {x:-1,y:0},
-            RIGHT => IVec2 {x:1,y:0},
-            DOWN => IVec2 {x:0,y:-1},
+            CardinalDirs::Up => IVec2 { x: 0, y: 1 },
+            CardinalDirs::Left => IVec2 { x: -1, y: 0 },
+            CardinalDirs::Right => IVec2 { x: 1, y: 0 },
+            CardinalDirs::Down => IVec2 { x: 0, y: -1 },
         }
     }
 }
 
-impl<T> Index<CardinalDirs> for [T;4] {
+impl<T> Index<CardinalDirs> for [T; 4] {
     type Output = T;
 
     fn index(&self, index: CardinalDirs) -> &Self::Output {
@@ -88,7 +204,7 @@ impl<T> Index<CardinalDirs> for [T;4] {
     }
 }
 
-impl<T> IndexMut<CardinalDirs> for [T;4] {
+impl<T> IndexMut<CardinalDirs> for [T; 4] {
     fn index_mut(&mut self, index: CardinalDirs) -> &mut Self::Output {
         let index: usize = index.into();
         return &mut self[index];
--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -1,68 +1,112 @@
-use image::{RgbaImage, Rgba};
-use std::collections::HashMap;
 use glam::UVec2;
+use image::{Rgba, RgbaImage};
+use std::collections::HashMap;
 
-use crate::adjacency_rules::{AdjacencyRules, CardinalDirs::{LEFT, DOWN, UP, RIGHT}};
+use crate::{adjacency_rules::{
+    AdjacencyRules,
+    CardinalDirs::{Down, Left},
+}, IdMap};
 
-type LocIdMap = HashMap<UVec2, usize>;
-type PatternIdMap = HashMap<Vec<Rgba<u8>>, usize>;
+/// The actual pixel data of the tile_size x tile_size rectangle (PatternRect)
+/// corresponding to a tile in the source image
+pub type Pattern = Vec<Rgba<u8>>;
+pub type RgbaPattern = Vec<u8>;
+type LocIdHMap = HashMap<UVec2, usize>;
+type PatternIdHMap = HashMap<RgbaPattern, usize>;
+// type IdPatternHMap = HashMap<usize, Pattern>;
 
 #[derive(Debug, Default)]
 pub struct PreProcessor {
     pub image: RgbaImage,
     pub tile_size: usize,
-    tiles: LocIdMap,
-    pattern_ids: PatternIdMap,
+    /// tiles_x , tiles_y
+    pub dims: UVec2,
+    /// Map of loc to Tile Id. Mainly for debugging
+    pub tile_loc_map: LocIdHMap,
+    /// the bottom left corner of the tile_size x tile_size pattern
+    /// in the source image corresponding to each unique tile
+    pub tiles: IdMap<UVec2>,
+    pattern_ids: PatternIdHMap,
 }
 
 impl PreProcessor {
-    // pub fn new(image: RgbaImage, tile_size: usize) -> Self {
-    //     return Self { image, tile_size};
-    // }
-    fn tile_locs(&self) -> Vec<UVec2> {
+    pub fn new(image: RgbaImage, tile_size: usize) -> Self {
+        let dims: UVec2 = image.dimensions().into();
+        return Self { image, tile_size, dims,..Default::default()};
+    }
+    pub fn num_tiles(&self) -> usize {
+        return self.tiles.len();
+    }
+    pub fn tile_ids(&self) -> impl Iterator<Item = usize> {
+        return 0..(self.num_tiles());
+    }
+    pub fn tile_locs(&self) -> Vec<UVec2> {
         let mut locs = Vec::new();
 
-        let dims: UVec2 = self.image.dimensions().into();
         // trim edges
-        let max = dims - (dims % self.tile_size as u32);
+        let max = self.dims - (self.dims % self.tile_size as u32);
 
         for x in (0..max.x).step_by(self.tile_size) {
             for y in (0..max.y).step_by(self.tile_size) {
-                locs.push(UVec2 {x,y});
+                locs.push(UVec2 { x, y });
             }
         }
 
         return locs;
     }
 
-    fn pattern_at(&self, loc: UVec2) -> Vec<Rgba<u8>> {
+    pub fn rgba_pattern_at(&self, loc: UVec2) -> RgbaPattern {
+        let mut pixels = Vec::with_capacity(self.tile_size * self.tile_size * 4);
+        let (min_x, min_y) = loc.into();
+        let (max_x, max_y) = (loc + self.tile_size as u32).into();
+        for x in min_x..max_x {
+            for y in min_y..max_y {
+                // pixels.push(self.image[(x, y)].into());
+                let mut rgba: Vec<u8> = self.image[(x,y)].0.to_owned().to_vec();
+                pixels.append(&mut rgba);
+            }
+        }
+        return pixels;
+    }
+
+    pub fn pattern_at(&self, loc: UVec2) -> Pattern {
         let mut pixels = Vec::with_capacity(self.tile_size * self.tile_size);
         let (min_x, min_y) = loc.into();
         let (max_x, max_y) = (loc + self.tile_size as u32).into();
         for x in min_x..max_x {
             for y in min_y..max_y {
-                pixels.push(self.image[(x,y)]);
+                pixels.push(self.image[(x, y)]);
+                // let mut rgba: Vec<u8> = self.image[(x,y)].0.to_owned().to_vec();
+                // pixels.append(&mut rgba);
             }
         }
         return pixels;
     }
-    // fn hash_tiles(&self, locs: Vec<UVec2>) -> HashMap<UVec2, 
 
-    fn process(&mut self) -> (Vec<usize>, AdjacencyRules) {
+    fn store_pattern_locs(&mut self) {
+        self.tiles = vec![UVec2::default(); self.pattern_ids.len()];
+        // unsorted vec of unique (id,pattern) entries
+        for (&loc, &id) in &self.tile_loc_map {
+            self.tiles[id] = loc;
+        }
+    }
+    // fn hash_tiles(&self, locs: Vec<UVec2>) -> HashMap<UVec2,
+
+    pub fn process(&mut self) -> (IdMap<usize>, AdjacencyRules) {
         // incremented to assign each tile a unique id
         let mut num_unique_tiles = 0;
-        // a map from tile pixels to it's tile id
-        let mut tile_freqs: Vec<usize> = Vec::new();
+        // a map from tile pixels to it's tile id let mut tile_freqs: Vec<usize> = Vec::new();
         let mut adj_rules = AdjacencyRules::new();
+        let mut tile_freqs: IdMap<usize> = IdMap::new();
 
         // iter over tile locations and store tile pixels
         // and keep track of unique tiles
         let locs = self.tile_locs();
         for loc in locs {
-            let pattern = self.pattern_at(loc);
+            let pattern = self.rgba_pattern_at(loc);
             let new_tile: bool = !self.pattern_ids.contains_key(&pattern);
             if new_tile {
-                self.pattern_ids.insert(pattern.clone(),num_unique_tiles);
+                self.pattern_ids.insert(pattern.clone(), num_unique_tiles);
                 num_unique_tiles += 1;
             }
             if !new_tile {
@@ -71,28 +115,30 @@ impl PreProcessor {
                 tile_freqs.push(1);
             }
             let id = self.pattern_ids[&pattern];
-            self.tiles.insert(loc,id);
+            self.tile_loc_map.insert(loc, id);
 
             // construct adjacency rules
             // locs are in column major order so left (<) and below (v) tiles are already extracted
             // add the adjacency rules in these directions if not on an edge
             let on_left_edge = loc.x == 0;
             let on_bottom_edge = loc.y == 0;
+            let tsize = self.tile_size as u32;
             if !on_left_edge {
-                let left_loc = loc - UVec2 { x:self.tile_size as u32, y:0 };
-                let left_id = self.tiles[&left_loc];
-                adj_rules.allow(id, left_id, LEFT);
+                let left_loc = loc - UVec2 { x: tsize, y: 0 };
+                let left_id = self.tile_loc_map[&left_loc];
                 // and reverse
-                adj_rules.allow(left_id, id, RIGHT);
+                adj_rules.allow_bi(id, left_id, Left);
             }
             if !on_bottom_edge {
-                let bottom_loc = loc - UVec2 { x: 0, y:self.tile_size as u32};
-                let bottom_id = self.tiles[&bottom_loc];
-                adj_rules.allow(id, bottom_id, DOWN);
+                let bottom_loc = loc - UVec2 { x: 0, y: tsize };
+                let bottom_id = self.tile_loc_map[&bottom_loc];
                 // and reverse
-                adj_rules.allow(bottom_id, id, UP);
+                adj_rules.allow_bi(id, bottom_id, Down);
             }
         }
+        self.store_pattern_locs();
+        assert!(!self.pattern_ids.is_empty());
+        assert!(!self.tiles.is_empty());
         return (tile_freqs, adj_rules);
     }
 }
@@ -102,22 +148,18 @@ mod test {
     use super::*;
 
     fn make_checkerboard(dims: UVec2) -> RgbaImage {
-        let white = [255,255,255,255];
-        let black = [0,0,0,255];
+        let white = [255, 255, 255, 255];
+        let black = [0, 0, 0, 255];
         let bside = black.repeat(4);
         let wside = white.repeat(4);
         let mut row = Vec::new();
         let mut worb: bool = false;
         for _w in (0..dims.x).step_by(4) {
-            let mut color = if worb {
-                wside.clone()
-            } else {
-                bside.clone()
-            };
+            let mut color = if worb { wside.clone() } else { bside.clone() };
             worb = !worb;
             row.append(&mut color);
         }
-        let mut odd_row = row.clone(); 
+        let mut odd_row = row.clone();
         odd_row.rotate_left(16);
         let mut color = white;
         let mut odd_color = black;
@@ -129,17 +171,22 @@ mod test {
         odd_row.append(&mut odd_color.repeat((dims.x % 4) as usize));
         let mut img_vec = Vec::new();
         // TODO: fill remaining rows if dims.y % 8 != 0
-        for _h in 0..(dims.y/8) {
+        for _h in 0..(dims.y / 8) {
             img_vec.append(&mut row.clone().repeat(4));
             img_vec.append(&mut odd_row.clone().repeat(4));
         }
-        return RgbaImage::from_vec(dims.x, dims.y, img_vec).expect("size of make_checkerboard correct");
+        return RgbaImage::from_vec(dims.x, dims.y, img_vec)
+            .expect("size of make_checkerboard correct");
     }
 
     fn checker_proc() -> PreProcessor {
-        let image = make_checkerboard(UVec2 {x:16,y:16});
+        let image = make_checkerboard(UVec2 { x: 16, y: 16 });
         let tile_size = 4;
-        return PreProcessor {image, tile_size, ..Default::default()};
+        return PreProcessor {
+            image,
+            tile_size,
+            ..Default::default()
+        };
     }
 
     #[test]
@@ -147,13 +194,13 @@ mod test {
         let mut proc = checker_proc();
         let (tile_freqs, _adj_rules) = proc.process();
         let pattern_ids = proc.pattern_ids;
-        let tiles = proc.tiles;
+        let tiles = proc.tile_loc_map;
         // tile ids
         assert_eq!(pattern_ids.len(), 2);
         let mut ids: Vec<&usize> = pattern_ids.values().collect();
         ids.sort();
-        assert_eq!(ids[0],&0);
-        assert_eq!(ids[1],&1);
+        assert_eq!(ids[0], &0);
+        assert_eq!(ids[1], &1);
 
         // tile_freqs
         assert_eq!(tile_freqs.iter().sum::<usize>(), 16);
@@ -171,15 +218,15 @@ mod test {
         let (_tile_freqs, adj_rules) = proc.process();
         for i in 0..4 {
             for j in 0..3 {
-                print!("{}", proc.image[(i*4, j*4)][0] / 255);
+                print!("{}", proc.image[(i * 4, j * 4)][0] / 255);
             }
-            println!("{}", proc.image[(i*4, 3*4)][0] / 255);
+            println!("{}", proc.image[(i * 4, 3 * 4)][0] / 255);
         }
         proc.image.save("./checker.png").unwrap();
         // println!("{:?}", proc.image.pixels().step_by(4).map(|p| p[0]/255).collect::<Vec<u8>>());
         println!("{adj_rules:?}");
         assert_eq!(adj_rules.len(), 2);
-        assert!(adj_rules.allowed_in_all_dirs(0,1));
-        assert!(adj_rules.allowed_in_all_dirs(1,0));
+        assert!(adj_rules.allowed_in_all_dirs(0, 1));
+        assert!(adj_rules.allowed_in_all_dirs(1, 0));
     }
 }
